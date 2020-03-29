4be519f128334ad9375658a7c6c6118448bf6973
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-29 20:45:58 +0300

    Add project files.

---
+++ InMemoryApp.Web/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemoryApp.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IMemoryCache _memoryCache;
+
+        public ProductController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
