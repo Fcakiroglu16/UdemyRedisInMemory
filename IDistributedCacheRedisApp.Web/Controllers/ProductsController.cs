ff6cf80097ce88765ba94d6f81e324404017609d
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-31 04:10:19 +0300

    IDistributedCache-1

---
+++ IDistributedCacheRedisApp.Web/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace IDistributedCacheRedisApp.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private IDistributedCache _distributedCache;
+
+        public ProductsController(IDistributedCache distributedCache)
+        {
+            _distributedCache = distributedCache;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            DistributedCacheEntryOptions cacheEntryOptions = new DistributedCacheEntryOptions();
+
+            cacheEntryOptions.AbsoluteExpiration = DateTime.Now.AddMinutes(1);
+
+            _distributedCache.SetString("name", "Fatih", cacheEntryOptions);
+            await _distributedCache.SetStringAsync("surname", "Çakıroğlu");
+            return View();
+        }
+
+        public IActionResult Show()
+        {
+            string name = _distributedCache.GetString("name");
+            ViewBag.name = name;
+            return View();
+        }
+
+        public IActionResult Remove()
+        {
+            _distributedCache.Remove("name");
+
+            return View();
+        }
+    }
+}
