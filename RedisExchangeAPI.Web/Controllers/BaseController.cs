ba3dcfbd1428e3a3e96942a9af5adf9d5752be04
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2020-03-31 20:37:08 +0300

    StackExchange.Redis API: Redis Hash

---
+++ RedisExchangeAPI.Web/Controllers/BaseController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        private readonly RedisService _redisService;
+
+        protected readonly IDatabase db;
+
+        public BaseController(RedisService redisService)
+        {
+            _redisService = redisService;
+            db = _redisService.GetDb(1);
+        }
+    }
+}
---
+++ RedisExchangeAPI.Web/Controllers/HashTypeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedisExchangeAPI.Web.Services;
+using StackExchange.Redis;
+
+namespace RedisExchangeAPI.Web.Controllers
+{
+    public class HashTypeController : BaseController
+    {
+        public string hashKey { get; set; } = "sozluk";
+
+        public HashTypeController(RedisService redisService) : base(redisService)
+        {
+        }
+
+        public IActionResult Index()
+        {
+            Dictionary<string, string> list = new Dictionary<string, string>();
+
+            if (db.KeyExists(hashKey))
+            {
+                db.HashGetAll(hashKey).ToList().ForEach(x =>
+                {
+                    list.Add(x.Name, x.Value);
+                });
+            }
+
+            return View(list);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string name, string val)
+        {
+            db.HashSet(hashKey, name, val);
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteItem(string name)
+        {
+            db.HashDelete(hashKey, name);
+            return RedirectToAction("Index");
+        }
+    }
+}
