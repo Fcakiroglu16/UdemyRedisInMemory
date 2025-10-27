ae6c334de9d587e89a1f636a803e8b2985a9c569
fcakiroglu16 <f-cakiroglu@outlook.com>
2025-10-27 12:02:02 +0300

    Add Redis stream publisher and consumer services
    Added `RedisConsumerBackgroundServiceErrorExample` to handle
    messages from the `my-stream-error` Redis stream. Introduced
    new API endpoints `/api/redis-stream-publisher` and
    `/api/redis-stream-publisher-error` for publishing messages
    to Redis streams. Updated `/api/redis-check` endpoint to
    remove unused constant.
    
    Added `RedisExample.http` examples for testing the new
    endpoints. Implemented advanced Redis stream processing
    with consumer group creation, message reading, and
    acknowledgment in the new background service.

--- RedisExample/Program.cs
+++ RedisExample/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddSingleton<RedisService>(sp =>
 });
 builder.Services.AddHostedService<RedisConsumerBackgroundService>();
 builder.Services.AddHostedService<RedisConsumerBackgroundService2>();
+builder.Services.AddHostedService<RedisConsumerBackgroundServiceErrorExample>();
 var app = builder.Build();
 
 
@@ -29,12 +30,32 @@ app.MapGet("api/redis-check", (RedisService redisService) =>
     return Results.Ok(value.ToString());
 });
 
-const string StreamName = "my-stream";
+
 app.MapGet("api/redis-stream-publisher", (RedisService redisService) =>
 {
     var db = redisService.GetDb(1);
 
+    const string StreamName = "my-stream";
+    Enumerable.Range(1, 100).ToList().ForEach(async void (i) =>
+    {
+        var values = new[]
+        {
+            new NameValueEntry("message_content", $"Hello world {i}"),
+            new NameValueEntry("version", "v1")
+        };
 
+
+        // StreamAddAsync, XADD komutunu çalıştırır.
+        // '*' ID'nin Redis tarafından otomatik oluşturulmasını sağlar.
+        var messageId = await db.StreamAddAsync(StreamName, values);
+    });
+});
+
+app.MapGet("api/redis-stream-publisher-error", (RedisService redisService) =>
+{
+    var db = redisService.GetDb(1);
+
+    const string StreamName = "my-stream-error";
     Enumerable.Range(1, 100).ToList().ForEach(async void (i) =>
     {
         var values = new[]
